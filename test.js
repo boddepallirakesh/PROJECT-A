7db806f966c84945943a0e99a2e1afe88303dca3
root <root@ip-10-0-4-56.ap-south-1.compute.internal>
2025-03-21 11:01:19 +0000

    1

---
+++ test.js
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Executes a greeting.
+ *
+ * This file is intended to run logic with side effects
+ * and does not declare any new symbols in the global scope.
+ */
+
+(function () {
+    echo "Hello, World!\n";
+})();
